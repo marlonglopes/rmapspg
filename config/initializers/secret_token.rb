f81f1eb3d2b03f7cc33a5a8ab28dc69db9e96983
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-10-21 16:59:34 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmapspg::Application.config.secret_token = '1d74908f96db5004dd395bca700b3b0bbf03a8236e6bf5a0185fb183a9b30726087430c3cc5ae1f0b80c3f9dc480b2f87e9e4cf5eae27253a27db87d8958a9a0'
